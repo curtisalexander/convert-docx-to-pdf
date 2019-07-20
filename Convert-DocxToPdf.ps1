158f45c474cf349a12e82dee1b26b294d8621ea9
Curtis Alexander <calex@calex.org>
2019-07-20 12:58:34 -0500

    Update README and complete script

--- Convert-DocxToPdf.ps1
+++ Convert-DocxToPdf.ps1
@@ -1,14 +1,19 @@
 <#
 .SYNOPSIS
 Convert-DocxToPdf converts a directory of Word documents (docx) into pdfs
+
 .DESCRIPTION
-Convert-DocxToPdf converts a directory of Word documents (docx) into pdfs
+Convert-DocxToPdf converts a directory of Word documents (docx) into pdfs.  The resulting pdf files will have the same filename as the Word documents.
+
 .PARAMETER docxDir
 The full path to the directory containing the Word documents
+
 .EXAMPLE
 Convert-DocxToPdf -docxDir "/Users/myusername/Documents/Word"
+
 .LINK
 https://github.com/curtisalexander/convert-docx-to-pdf
+
 .NOTES
 Author: Curtis Alexander
 #>
@@ -26,5 +31,26 @@ Param(
     [System.IO.FileInfo]$docxDir
 )
 
-$wordObject = New-Object -ComObject Word.Application
-$wordObject | Get-Member
+function convertToPdf {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [System.__ComObject]$wordApp,
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo]$wordFile
+    )
+    $pdfFile = Join-Path $wordFile.DirectoryName "$($wordFile.Basename).pdf" 
+    $wordDoc = $wordApp.Documents.Open($wordFile.FullName)
+    Write-Host "Converting $wordFile to $pdfFile"
+    try {
+        $wordDoc.SaveAs($pdfFile, 17)
+    }
+    finally {
+        $wordDoc.Close()
+    }
+}
+
+$wordApp = New-Object -ComObject Word.Application
+Get-ChildItem -Path "${docxDir}" -Filter "*.docx" | ForEach-Object {
+    convertToPdf -wordApp $wordApp -wordFile $_
+}
+$wordApp.Quit()
